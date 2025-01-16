0f6c1b592d20781fc458046a8df445073e7fba81
Denis S Dujota <denisdujota@gmail.com>
2025-01-17 02:14:07 +0300

    setup jwt

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-# app.py
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 # Import the 'Flask' class from the 'flask' library.
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import jwt
 
 # Initialize Flask
 # We'll use the pre-defined global '__name__' variable to tell Flask where it is.
@@ -19,7 +23,18 @@ def sign_token():
         "username": "test",
         "password": "test"
     }
-    return jsonify(user)
+
+    token = jwt.encode(user, os.getenv('JWT_SECRET'), algorithm="HS256")
+    return jsonify({"token": token})
+
+@app.route('/verify-token', methods=['POST'])
+def verify_token():
+    try:
+      token = request.headers.get('Authorization').split(' ')[1]
+      decoded_token = jwt.decode(token, os.getenv('JWT_SECRET'), algorithms=["HS256"])
+      return jsonify({"user": decoded_token})
+    except Exception as error:
+       return jsonify({"error": "unauthorized" })
 
 # Run our application, by default on port 5000
 app.run()
